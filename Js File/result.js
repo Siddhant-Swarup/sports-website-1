f66e6ccc2ee480555f6898907d41e7ebacf0c96f
Siddhant-Swarup <swarupsiddhant311@gmail.com>
2021-06-27 11:06:46 +0530

    Sports Website 3

---
+++ Js File/result.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC7hT0tHLl0qOPxRU9k0DdrqCz0KOM37sw",
+    authDomain: "database-b8ffb.firebaseapp.com",
+    databaseURL: "https://database-b8ffb-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "database-b8ffb",
+    storageBucket: "database-b8ffb.appspot.com",
+    messagingSenderId: "1015425565984",
+    appId: "1:1015425565984:web:4782b90e2dbd23bb254345"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
