71aa1eaf02b570b4c7d82ae6c031cce29b8ae8e7
Siddhant-Swarup <swarupsiddhant311@gmail.com>
2021-06-27 19:06:30 +0530

    Final version

--- Js File/result.js
+++ Js File/result.js
@@ -7,6 +7,31 @@ var firebaseConfig = {
     storageBucket: "database-b8ffb.appspot.com",
     messagingSenderId: "1015425565984",
     appId: "1:1015425565984:web:4782b90e2dbd23bb254345"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+var selection = document.querySelector('select');
+
+var temp, value=0;
+selection.addEventListener('change', () => {
+    temp = selection.options[selection.selectedIndex].text;
+    value = parseInt(temp);
+    console.log(value)
+    readResult(value);
+    
+});
+
+function readResult(value) {
+    var result=firebase.database().ref().child(value);
+    result.on('value',(data) => {
+        
+        console.log(data.val())
+        document.getElementById('Object').innerHTML="Loading";
+        document.getElementById("Object").innerHTML= data.val();
+    });
+    
+}
+
+
